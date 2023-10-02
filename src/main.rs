42f4d7680b50d822bd689de4ca5e02238ba0885a
Praveen Perera <praveen@avencera.com>
2023-10-02 00:13:56 -0400

    Use rayon

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
+use std::sync::{Arc, Mutex};
+
+use derive_more::{Deref, From};
 use itertools::Itertools;
+use rayon::prelude::*;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize, derive_more::From, derive_more::Deref)]
+#[derive(Debug, Clone, Serialize, Deserialize, From, Deref)]
 pub struct Words(Vec<Vec<String>>);
 
 fn main() {
     let words_json = std::env::var("WORDS").unwrap();
     let words: Words = serde_json::from_str(&words_json).unwrap();
 
-    let mut combos = Vec::new();
+    let combos = Arc::new(Mutex::new(Vec::new()));
     let words_len = words.len();
 
-    for prod in words.0.into_iter().multi_cartesian_product() {
-        for p in prod.into_iter().permutations(words_len) {
-            let combo = p.join(" ");
-            combos.push(combo);
-        }
-    }
+    words
+        .0
+        .into_iter()
+        .multi_cartesian_product()
+        .par_bridge()
+        .for_each(|prod| {
+            prod.into_iter()
+                .permutations(words_len)
+                .par_bridge()
+                .for_each(|p| {
+                    let combo = p.join(" ");
+                    combos.lock().unwrap().push(combo);
+                });
+        });
 
-    println!("{}", combos.len());
+    println!("{}", combos.lock().unwrap().len());
 }
