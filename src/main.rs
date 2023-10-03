31793f43d33ab90372f600f96c6e29d6cf3851a6
Praveen Perera <praveen@avencera.com>
2023-10-02 20:28:42 -0400

    Add test to confirm its working
    Remove permutation test from main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::sync::{Arc, Mutex};
 
 use derive_more::{Deref, From};
 use itertools::Itertools;
@@ -8,27 +8,52 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize, From, Deref)]
 pub struct Words(Vec<Vec<String>>);
 
+impl From<Vec<Vec<&str>>> for Words {
+    fn from(words: Vec<Vec<&str>>) -> Self {
+        let words: Vec<Vec<String>> = words
+            .into_iter()
+            .map(|v| v.into_iter().map(|s| s.to_string()).collect())
+            .collect();
+        Words(words)
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct App {
     pub seed: String,
     pub words: Words,
+    pub fingerprint: [u8; 4],
 }
 
 pub const FINGERPRINT: &str = "1cf84a2a";
 
-impl App {
-    pub fn new() -> Self {
+impl Default for App {
+    fn default() -> Self {
         let words_json = std::env::var("WORDS").unwrap();
+
         let words: Words = serde_json::from_str(&words_json).unwrap();
         let seed = std::env::var("SEED").unwrap();
+        let target_fingerprint = hex::decode(FINGERPRINT).unwrap()[..4].try_into().unwrap();
 
-        Self { words, seed }
+        Self::new(words, seed, target_fingerprint)
+    }
+}
+
+impl App {
+    pub fn new(words: Words, seed: String, fingerprint: [u8; 4]) -> Self {
+        Self {
+            words,
+            seed,
+            fingerprint,
+        }
     }
 
-    pub fn run(self, sender: crossbeam::channel::Sender<(String, [u8; 4])>) {
+    pub fn run(self) -> Option<String> {
         let words_len = self.words.len();
-        let target_fingerprint = hex::decode(FINGERPRINT).unwrap();
+        let target_fingerprint: [u8; 4] = self.fingerprint;
+
         let seed = &self.seed;
+        let found = Arc::new(Mutex::new(None));
 
         self.words
             .0
@@ -45,44 +70,49 @@ impl App {
                             combinator::fingerprint::create_fingerprint(seed, &passphrase_string)
                                 .unwrap();
 
-                        if target_fingerprint.as_slice() == fingerprint {
+                        if target_fingerprint == fingerprint {
                             println!(
                                 "Found fingerprint: {}",
                                 hex::encode(target_fingerprint.clone())
                             );
-                            println!("Passphrase: {}", passphrase.join(" "));
-                        }
 
-                        // sender.send((passphrase_string, fingerprint)).unwrap();
+                            println!("Passphrase: {}", passphrase.join(" "));
+                            *found.lock().unwrap() = Some(passphrase.join(" "));
+                        }
                     });
             });
+
+        if let Some(passphrase) = found.lock().unwrap().clone() {
+            return Some(passphrase);
+        }
+
+        None
     }
 }
 
 fn main() {
     // let combos = Arc::new(Mutex::new(0));
     // let combos_clone = combos.clone();
-    //
 
-    let (sender, receiver) = crossbeam::channel::bounded(1000);
+    // let (sender, receiver) = crossbeam::channel::bounded(1000);
     let app_thread = std::thread::spawn(move || {
-        let app = App::new();
-        app.run(sender);
+        let app = App::default();
+        app.run();
     });
 
-    let writer_thread = std::thread::spawn(move || {
-        let file = std::fs::File::create("output.txt").unwrap();
-        let mut writer = std::io::BufWriter::new(file);
-
-        for (passphrase, fingerprint) in receiver.iter() {
-            writer.write_all(passphrase.as_bytes()).unwrap();
-            writer.write_all(b"=").unwrap();
-            writer.write_all(&fingerprint).unwrap();
-            writer.write_all(b"\n").unwrap();
-        }
-
-        writer.flush().unwrap();
-    });
+    // let writer_thread = std::thread::spawn(move || {
+    //     let file = std::fs::File::create("output.txt").unwrap();
+    //     let mut writer = std::io::BufWriter::new(file);
+    //
+    //     for (passphrase, fingerprint) in receiver.iter() {
+    //         writer.write_all(passphrase.as_bytes()).unwrap();
+    //         writer.write_all(b"=").unwrap();
+    //         writer.write_all(&fingerprint).unwrap();
+    //         writer.write_all(b"\n").unwrap();
+    //     }
+    //
+    //     writer.flush().unwrap();
+    // });
 
     // let running = Arc::new(AtomicBool::new(true));
     // let r = running.clone();
@@ -102,5 +132,5 @@ fn main() {
     app_thread.join().unwrap();
     println!("App thread joined");
 
-    writer_thread.join().unwrap();
+    // writer_thread.join().unwrap();
 }
